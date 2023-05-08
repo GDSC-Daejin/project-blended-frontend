b4b224ec1981d8b3212d75bc49ec8e9c55d25cd3
Sujin <tnwlsk0190@gmail.com>
2023-05-08 20:48:40 +0900

    🛠 종속성 문제 해결
    styled-components

---
+++ firebase_config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA7GSeuJngysIsY7rJvMKEQTxwd46_Uwg0",
+    authDomain: "blended-a4e0a.firebaseapp.com",
+    projectId: "blended-a4e0a",
+    storageBucket: "blended-a4e0a.appspot.com",
+    messagingSenderId: "829479690496",
+    appId: "1:829479690496:web:49b5b2ba9f061adde853e5",
+    measurementId: "G-5DETJ1JF74"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export default class firebaseConfig {
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
 import MainPage from './pages/MainPage/index.jsx';
-import { Reset } from 'styled-reset'
+import { Reset } from 'styled-reset';
+// import firebase_config from '/firebase_config.js';
 
 function App() {
+    // console.log(firebase_config)
   return (
     <>
       <Reset />
